0348d4e8b079a5d1878a1e1a029e49747753cb21
Cláudio Oliveira <franciscoclaudiooliveira@gmail.com>
2023-11-20 22:14:10 -0300

    inserino itens no local storage

--- scripts.js
+++ scripts.js
@@ -6,7 +6,7 @@ const todoApp = {
             todos: [
                 
             ],
-            newTodo: {}
+            newTodo: {done: false}
         }
     },
     methods: {
@@ -15,14 +15,20 @@ const todoApp = {
                 return alert("Você precisa digitar algo!");
             }
             this.todos.push(this.newTodo);
-            this.newTodo = {};
+            this.newTodo = {done: false};
+            localStorage.setItem('todos', JSON.stringify(this.todos));
         },
         removeTodo: function() {
             this.todos = [];
+            localStorage.setItem('todos', JSON.stringify(this.todos));
         },
         markAsDone: function(todo) {
             todo.done = !todo.done;
+            localStorage.setItem('todos', JSON.stringify(this.todos));
         },
+    },
+    created() {
+        this.todos = JSON.parse(localStorage.getItem('todos')) || [];
     },
 };
 
